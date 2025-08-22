b933b64b1929c9ae9dc5981464e03e50817d1f4a
Divyansh Sharma <divyansh.sharma@iamtechie.com>
2025-08-22 18:38:04 +0530

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import searchRoutes from "./routes/search.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
